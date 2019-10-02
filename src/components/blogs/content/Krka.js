f4f2c48068a34921cb92fea9a6a3b2edb775257f
equidistant <mich.random@pm.me>
2019-10-02 14:12:59 +0200

    krka images overline text

--- src/components/blogs/content/Krka.js
+++ src/components/blogs/content/Krka.js
@@ -42,11 +42,11 @@ const Krka = () => {
           </ImgRow>
           <ImgRow>
             <ImgWrapLandscapeLeft>
-              <ImgOverlay>Ćorkica u hammocku</ImgOverlay>
+              <ImgOverlay>Patkice idu na spavanje</ImgOverlay>
               <ImgLandscapeHalf img={krka[8]}/>
             </ImgWrapLandscapeLeft>
             <ImgWrapLandscape>
-              <ImgOverlay>I pod je sasvim ok...</ImgOverlay>
+              <ImgOverlay>Plaža</ImgOverlay>
               <ImgLandscapeHalf img={krka[9]}/>
             </ImgWrapLandscape>
           </ImgRow>
@@ -57,8 +57,14 @@ const Krka = () => {
             Nakon jutarnjeg mira na plaži u NP ostatak subote odlučili smo provesti u nešto avanturističkijem duhu. Od ekipe iz agencije Raftrek rentali smo sit on top kajak za dvoje, preuzeli ga na plaži Zablaće i uputili se u našu prvu morsku kajak avanturu.
           </Text>
           <Text>
-            Prvi dojmovi su odlični (Mislav još razmišlja o neudobnosti sjedalice, ali naviknut će se s vremenom). Naš pametni sat kaže da smo veslali 10ak kilometara (za usporedbu, krug oko Zlarina je 14km). Prilikom izvlačenja kayaka na kraju dana, shvatili smo da od samog jutra imamo odčepljen otvor za vodu tako da cijel dan veslamo s hendikepom (jeej).
+            Prvi dojmovi su odlični (Mislav još razmišlja o neudobnosti sjedalice, ali naviknut će se s vremenom). Naš pametni sat kaže da smo veslali 10ak kilometara (za usporedbu, krug oko Zlarina je 14km). Prilikom izvlačenja kayaka na kraju dana, shvatili smo da od samog jutra imamo odčepljen otvor za vodu tako da čitav dan veslamo s hendikepom (jeej).
           </Text>
+          <ImgRow>
+            <ImgWrap>
+              <ImgOverlay>Tu jos ne znamo da imamo rupu u kayaku!</ImgOverlay>
+              <Img img={krka[0]}/>
+            </ImgWrap>
+          </ImgRow>
           <Text>
             Prvo smo locirali uvalu preko puta do koje nam je trebalo 20ak min vožnje. Tamo se malo odmorili i nastavili vožnju oko otoka prema samom gradu Zlarinu. Htjeli smo vidjeti kakva je i druga strana otoka koja gleda prema moru.
           </Text>
@@ -66,14 +72,10 @@ const Krka = () => {
             Već nakon prvih nekoliko zavoja shvatili smo da je druga strana vrlo stjenovita i neprilagođena odmoru pa smo se okrenuli i nasukali na jednu od već viđenih uvala. Smjestili smo se u hammock i ulovili još malo mira.  Divan li je taj naš Jadran u rujnu. Avantura s kayakom na moru bila je uspješna. Sigurno ćemo ponoviti!
           </Text>
           <ImgRow>
-            <ImgWrapLandscapeLeft>
-              <ImgOverlay>Ćorkica u hammocku</ImgOverlay>
-              <ImgLandscapeHalf img={krka[0]}/>
-            </ImgWrapLandscapeLeft>
-            <ImgWrapLandscape>
-              <ImgOverlay>I pod je sasvim ok...</ImgOverlay>
-              <ImgLandscapeHalf img={krka[1]}/>
-            </ImgWrapLandscape>
+            <ImgWrap>
+              <ImgOverlay>Mmmmm... to je taj mir</ImgOverlay>
+              <Img img={krka[1]}/>
+            </ImgWrap>
           </ImgRow>
           <BulletHeaderContainer>
             <StarBullet/><Header3>NP Krka</Header3>
@@ -86,11 +88,11 @@ const Krka = () => {
           </Text>
           <ImgRow>
             <ImgWrapLandscapeLeft>
-              <ImgOverlay>Ćorkica u hammocku</ImgOverlay>
+              <ImgOverlay>Jedna klasična</ImgOverlay>
               <ImgLandscapeHalf img={krka[3]}/>
             </ImgWrapLandscapeLeft>
             <ImgWrapLandscape>
-              <ImgOverlay>I pod je sasvim ok...</ImgOverlay>
+              <ImgOverlay>Šetamo polako pored rijeke</ImgOverlay>
               <ImgLandscapeHalf img={krka[4]}/>
             </ImgWrapLandscape>
           </ImgRow>
@@ -99,7 +101,7 @@ const Krka = () => {
           </Text>
           <ImgRow>
             <ImgWrap>
-              <ImgOverlay>Napokon neka ravna podloga da se slikamo</ImgOverlay>
+              <ImgOverlay>Ana je digla drona</ImgOverlay>
               <Img img={krka[5]}/>
             </ImgWrap>
           </ImgRow>
