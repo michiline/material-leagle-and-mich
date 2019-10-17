9577d4115a3c45a2f235db79fd585b6475530be1
equidistant <mich.random@pm.me>
2019-10-17 15:12:56 +0200

    image grid modification

--- src/components/general/ImageGrid.js
+++ src/components/general/ImageGrid.js
@@ -3,14 +3,20 @@ import { withRouter } from 'react-router-dom'
 import styled, { css, keyframes } from 'styled-components'
 import { H3 } from './text'
 import { FlexColumnCenterContainer } from './containers'
-import { padding } from '../../style'
+import { padding, shadow } from '../../style'
 
 const ImageGrid = ({ history, header, url, content }) => {
   return (
     <Container>
       <Header onClick={ e => to({ history, url })}>{header}</Header>
       <Grid>
-        {content.map((image, index) => <Image {...image} key={index} />)}
+        {content.map((image, index) =>
+          <ImgContainer>
+            <ImgOverlay />
+            <OverlayRow>Cuba</OverlayRow>
+            <Image {...image} key={index} />
+          </ImgContainer>
+        )}
       </Grid>
     </Container>
   )
@@ -38,13 +44,17 @@ const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, calc((100vw - 5*${padding.small}) / 2));
   grid-template-rows: repeat(4, calc((100vw - 5*${padding.small}) / 2));
-  @media only screen and (min-width: 1000px) {
+  @media only screen and (min-width: 600px) {
     grid-template-columns: repeat(3, calc((100vw - 6*${padding.small}) / 3));
     grid-template-rows: repeat(3, calc((100vw - 6*${padding.small}) / 3));
   }
-  @media only screen and (min-width: 1400px) {
+  @media only screen and (min-width: 1000px) {
     grid-template-columns: repeat(4, calc((100vw - 7*${padding.small}) / 4));
     grid-template-rows: repeat(2, calc((100vw - 7*${padding.small}) / 4));
+  }
+  @media only screen and (min-width: 1400px) {
+    grid-template-columns: repeat(5, calc((100vw - 8*${padding.small}) / 5));
+    grid-template-rows: repeat(2, calc((100vw - 8*${padding.small}) / 5));
   }
   grid-gap: 1rem;
   align-items: center;
@@ -56,6 +66,8 @@ const Grid = styled.div`
 const ImgContainer = styled.div`
   width: 100%;
   height: 100%;
+  position: relative;
+  border: ${shadow.dp1};
 `
 
 const Img = styled.img.attrs(props => ({
@@ -64,7 +76,37 @@ const Img = styled.img.attrs(props => ({
   width: 100%;
   height: 100%;
   object-fit: cover;
+`
 
+const ImgOverlay = styled.button`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 50%;
+  left: 50%;
+  text-align: center;
+  transform: translate(-50%, -50%);
+  cursor: pointer;
+  z-index: 1;
+  opacity: 0.3;
+  background-color: #24272E;
+  border: none;
+`
+
+const OverlayRow = styled.p`
+  position: absolute;
+  bottom: 0;
+  font-size: 4.8rem;
+  letter-spacing: 0;
+  line-height: 1.6;
+  padding: ${padding.medium};
+  font-family: 'Amatic SC';
+  font-weight: 700;
+  &:active {
+    transform: translateY(2px);
+  }
+  color: white;
+  z-index: 1;
 `
 
 const Image = (props) =>
