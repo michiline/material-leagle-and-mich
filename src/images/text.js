199d7ca3d01e077f996c94dfd57ab38e61e8bf91
equidistant <mich.random@pm.me>
2019-10-30 22:22:30 +0100

    new galleries added, chronological order

---
+++ src/images/text.js
@@ -0,0 +1,137 @@
+export default {
+  'kamacnik': {
+    h1: 'Kamacnik',
+    h2: 'Kamacnik',
+    h3: 'Kamacnik'
+  },
+  'tara': {
+    h1: 'Tara',
+    h2: 'Tara',
+    h3: 'Tara'
+  },
+  'krka': {
+    h1: 'Krka',
+    h2: 'Krka',
+    h3: 'Krka'
+  },
+  'soca': {
+    h1: 'Soca',
+    h2: 'Soca',
+    h3: 'Soca'
+  },
+  'istra': {
+    h1: 'Istra',
+    h2: 'Istra',
+    h3: 'Istra'
+  },
+  'kalnik': {
+    h1: 'Kalnik',
+    h2: 'Kalnik',
+    h3: 'Kalnik'
+  },
+  'gerlitzen': {
+    h1: 'Gerlitzen',
+    h2: 'Gerlitzen',
+    h3: 'Gerlitzen'
+  },
+  'kuba': {
+    h1: 'Kuba',
+    h2: 'Kuba',
+    h3: 'Kuba'
+  },
+  'london': {
+    h1: 'London',
+    h2: 'London',
+    h3: 'London'
+  },
+  'budimpesta': {
+    h1: 'Budimpesta',
+    h2: 'Budimpesta',
+    h3: 'Budimpesta'
+  },
+  'novi': {
+    h1: 'Novi Vinodolski',
+    h2: 'Novi Vinodolski',
+    h3: 'Novi Vinodolski'
+  },
+  'mljet': {
+    h1: 'Mljet',
+    h2: 'Mljet',
+    h3: 'Mljet'
+  },
+  'lastovo': {
+    h1: 'Lastovo',
+    h2: 'Lastovo',
+    h3: 'Lastovo'
+  },
+  'vransko': {
+    h1: 'Vransko Jezero',
+    h2: 'Vransko Jezero',
+    h3: 'Vransko Jezero'
+  },
+  'zagreb': {
+    h1: 'Zagreb',
+    h2: 'Zagreb',
+    h3: 'Zagreb'
+  },
+  'tabor': {
+    h1: 'Veliki Tabor',
+    h2: 'Veliki Tabor',
+    h3: 'Veliki Tabor'
+  },
+  'plitvice': {
+    h1: 'Plitvicka Jezera',
+    h2: 'Plitvicka Jezera',
+    h3: 'Plitvicka Jezera'
+  },
+  'pariz': {
+    h1: 'Pariz',
+    h2: 'Pariz',
+    h3: 'Pariz'
+  },
+  'nassfeld': {
+    h1: 'Nassfeld',
+    h2: 'Nassfeld',
+    h3: 'Nassfeld'
+  },
+  'risnjak': {
+    h1: 'Risnjak',
+    h2: 'Risnjak',
+    h3: 'Risnjak'
+  },
+  'trakoscan': {
+    h1: 'Trakoscan',
+    h2: 'Trakoscan',
+    h3: 'Trakoscan'
+  },
+  'barcelona': {
+    h1: 'Barcelona',
+    h2: 'Barcelona',
+    h3: 'Barcelona'
+  },
+  'krupa': {
+    h1: 'Krupa',
+    h2: 'Krupa',
+    h3: 'Krupa'
+  },
+  'dugi': {
+    h1: 'Dugi Otok',
+    h2: 'Dugi Otok',
+    h3: 'Dugi Otok'
+  },
+  'brijuni': {
+    h1: 'Brijuni',
+    h2: 'Brijuni',
+    h3: 'Brijuni'
+  },
+  'mitrovica': {
+    h1: 'Sremska Mitrovica',
+    h2: 'Sremska Mitrovica',
+    h3: 'Sremska Mitrovica'
+  },
+  'fuzine': {
+    h1: 'Fuzine',
+    h2: 'Fuzine',
+    h3: 'Fuzine'
+  }
+}
